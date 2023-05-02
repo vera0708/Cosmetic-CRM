9f04aab1acc81b1131731a2de68c31da37876f17
vera0708 <verasdee@gmail.com>
2023-05-02 12:15:04 +0700

    init: create project

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const productName = 'lipstick';
+const productAmount = 4;
+const productCategory = "makeup";
+const productPrice = 1100;
+
+const TotalSum = productAmount * productPrice;
+
+console.log('productName');
+console.log(TotalSum);
