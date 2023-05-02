0b169bda412b1223007b176c7da67e94f9d63191
vera0708 <verasdee@gmail.com>
2023-05-02 20:31:27 +0700

    feat: einstein task and prompt-task

---
+++ einstein.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const myWeight = 65;
+const speedLight = 3e8;
+
+const einsteinEnergy = myWeight * Math.pow(speedLight, 2);
+
+console.log(Math.pow(speedLight, 2));
+
+console.log(`Энергия неопознанного тела ${einsteinEnergy} джоуль`);
--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 'use strict'
 
-const productName = 'lipstick';
-const productAmount = 4;
-const productCategory = "makeup";
-const productPrice = 1100;
+const productName = prompt('Введите наименование товара');
 
-const TotalSum = productAmount * productPrice;
+const productAmount = +prompt('Введите количество товара');
 
-console.log('productName');
-console.log(TotalSum);
+const productCategory = prompt('Укажите категорию товара');
+
+const productPrice = +prompt('Укажите желаемую цену товара');
+
+
+const totalSum = productAmount * productPrice;
+
+console.log('Тип переменной "количество товара"' + typeof (productAmount));
+console.log('Тип переменной "цену товара"' + typeof (productPrice));
+
+console.log(`Итого выбрано ${productAmount} единиц товара "${productName}" на общую сумму ${totalSum} рублей.`);
