b9f00582884396dbd0786c5130b90870be23d5d1
vera0708 <verasdee@gmail.com>
2023-05-08 13:22:57 +0700

    feat: task 3 lesson 5 module 4

--- js_lesson5/task0_03.js
+++ js_lesson5/task0_03.js
@@ -4,10 +4,10 @@
 const initString = prompt('Введите короткий текст, используя маленькие и заглавние буквы');
 
 const modifyString = (text) => {
-    let string = text.trimStart().toLowerCase();
-    return string[0].toUpperCase() + string.slice(1);
+    let string = text.split('').reverse().join('');
+    return string;
 }
 
 const modifiedString = modifyString(initString)
 
-console.log(`Ваша строка с большой буквы, остальные буквы маленькие: "${modifiedString}"`);
+console.log(`Ваша строка задом наперёд: "${modifiedString}"`);
