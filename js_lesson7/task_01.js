85f80cb0710a9e69f3c2d193236f27775c706128
vera0708 <verasdee@gmail.com>
2023-05-10 21:45:06 +0700

    feat: task 1 lesson 7 module 4

---
+++ js_lesson7/task_01.js
@@ -0,0 +1,22 @@
+'use strict'
+// Первая задача:
+
+const userNumber = +prompt('Введите желаемое количество элементов');
+
+const maxValue = 100;
+const randomArr = [];
+
+const generateRandomNum = (num) => {
+    for (let i = 0; i < num; i++) {
+
+        randomArr.push(Math.round(Math.random() * maxValue) + 1);
+    }
+    console.log(`Массив с ${num}-ю случайными числами от 1 до ${maxValue}: ${randomArr.join(', ')}`);
+}
+
+if (userNumber < 0 || Number.isFinite(userNumber) === false) {
+    console.log("Вы ввели некорректную информацию")
+} else {
+    generateRandomNum(userNumber);
+
+}
