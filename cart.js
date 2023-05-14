511a514a478d86762f988535807f99c9d30a7df3
vera0708 <verasdee@gmail.com>
2023-05-14 21:14:00 +0700

    feat: after fixing eslint  lesson 10 module 4

--- cart.js
+++ cart.js
@@ -1,39 +1,40 @@
+'use strict';
 // Первая задача:
 
 const cart = {
-    items: [],
-    get totalPrice() {
-        return this.realPrice;
-    },
+  items: [],
+  get totalPrice() {
+    return this.realPrice;
+  },
 
-    set totalPrice(sum) {
-        this.realPrice = sum;
-    },
-    realPrice: 0,
-    count: 0,
+  set totalPrice(sum) {
+    this.realPrice = sum;
+  },
+  realPrice: 0,
+  count: 0,
 
-    add(name, price, amount = 1) {
-        const product = {
-            name: name,
-            price: price,
-            amount: amount,
-        }
-        this.items.push(product);
-        this.count += amount;
-        this.totalPrice += price;
-    },
-    increaseCount(amoumt) {
-        this.count += amoumt;
-    },
-    clear() {
-        items = [];
-        totalPrice = 0;
-        count = 0;
-    },
-    print() {
-        console.log(JSON.stringify(this.items));
-        console.log(`Общая стоимость корзины: , ${this.totalPrice}`);
-    },
+  add(name, price, amount = 1) {
+    const product = {
+      name,
+      price,
+      amount,
+    };
+    this.items.push(product);
+    this.count += amount;
+    this.totalPrice += price;
+  },
+  increaseCount(amoumt) {
+    this.count += amoumt;
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(`Общая стоимость корзины: , ${this.totalPrice}`);
+  },
 };
 
 cart.add('lipstic', 16, 4);
@@ -42,7 +43,7 @@ cart.add('base', 30, 3);
 cart.add('mascara', 24, 2);
 cart.print();
 
-console.log(`cart.totalPrice: , ${cart.totalPrice}`)
+console.log(`cart.totalPrice: , ${cart.totalPrice}`);
 
 console.log('count: ', cart.count);
 
@@ -50,6 +51,5 @@ cart.increaseCount(3);
 console.log('count increased: ', cart.count);
 
 cart.clear();
-
 
 
